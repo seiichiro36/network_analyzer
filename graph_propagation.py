7141f7cbfed1459106dfcec294a2d6d912fd6088
Kouno <daigakuyouyyo@gmail.com>
2023-03-01 21:59:33 +0900

    add value

--- graph_propagation.py
+++ graph_propagation.py
@@ -8,15 +8,14 @@ from theorical import beta
 
 fig = plt.figure(figsize=(12, 8))
 # Frequency1 = np.linspace(60000, 30000000, 501)
-#np.linespaceからnp.arangeに変更
+# np.linespaceからnp.arangeに変更
 Frequency = np.arange(60000, 30000001, 59880)
 
+
 def linear_transform(phase_constant):
-        
     phase = []
     for i in range(0, len(phase_constant)):
         phase.append(phase_constant[i])
-
     Frequency_gamma = []
     for i in range(0, len(Frequency)):
         Frequency_gamma.append(Frequency[i])
@@ -25,7 +24,7 @@ def linear_transform(phase_constant):
     start_point = []
     end_point = []
     final_point = phase[-1]
-    
+
     total_break_point = []
 
     for j in range(0, len(phase_constant)):
@@ -37,7 +36,8 @@ def linear_transform(phase_constant):
             start_point.append(phase[j+1])
             end_point.append(phase[j])
 
-    between_phase = phase[phase.index(initial_point): phase.index(start_point[0])]
+    between_phase = phase[phase.index(
+        initial_point): phase.index(start_point[0])]
 
     break_point = phase.index(start_point[0])
     total_break_point.append(break_point)
@@ -55,20 +55,17 @@ def linear_transform(phase_constant):
         total_break_point.append(break_point)
         del Frequency_gamma[break_point]
 
-
     amount_of_change = start_point[-1]
     between_start_end_final = phase[phase.index(
         start_point[-1])+1: phase.index(final_point)+1]
     between_start_end_final = between_start_end_final + \
         between_phase[-1] + amount_of_change*-1
     between_phase.extend(between_start_end_final)
-    
-    phase = between_phase
-    
-    return Frequency_gamma, phase, total_break_point
 
-def delete_phase_constant_linear(phase_constant_linear):
-    pass
+    phase = between_phase
+
+    return Frequency_gamma, phase, total_break_point, start_point, end_point
+
 
 open = Open("OPEN.CSV")
 open_complex_data = open.open_parameter()
@@ -86,48 +83,64 @@ phase_constant = propagation_constant[1]
 Frequency_gamma = linear_transform(phase_constant)[0]
 phase_constant_linear = linear_transform(phase_constant)[1]
 
-# linear_transform(phase_constant)[2]
+start_point = linear_transform(phase_constant)[3]
 
-deleted_phase_constant_linear = phase_constant_linear.copy()
+end_point = linear_transform(phase_constant)[4]
 
-for k in range(len(linear_transform(phase_constant)[2])):
-    del deleted_phase_constant_linear[linear_transform(phase_constant)[k]]
+ax1 = fig.add_subplot(1, 2, 1)
+ax1.set_xlabel("(a)   Frequency [Hz]")
+ax1.set_ylabel("[Np/m]")
+
+ax2 = fig.add_subplot(1, 1, 1)
+ax2.set_xlabel("(b)   Frequency [Hz]")
+ax2.set_ylabel("[rad/m]")
+
+beta1 = beta[0: phase_constant.tolist().index(start_point[0])]
+Frequency1 = Frequency[0: phase_constant.tolist().index(start_point[0])]
+
+beta2 = beta[phase_constant.tolist().index(start_point[0]): phase_constant.tolist().index(start_point[1])] - beta[phase_constant.tolist().index(start_point[0])] - beta[phase_constant.tolist().index(start_point[0])]
     
-print(len(deleted_phase_constant_linear))
-    
-    
-# for k in range():
-#     val = np.sqrt(abs(phase_constant_linear - beta)**2)
-#     dist.append(val)
+Frequency2 = Frequency[phase_constant.tolist().index(
+    start_point[0]): phase_constant.tolist().index(start_point[1])]
 
-# ax1 = fig.add_subplot(1, 2, 1)
-# ax1.set_xlabel("(a)   Frequency [Hz]")
-# ax1.set_ylabel("[Np/m]")
+beta3 = beta[phase_constant.tolist().index(start_point[1]): phase_constant.tolist().index(start_point[2])] - beta[phase_constant.tolist().index(start_point[1])] - beta[phase_constant.tolist().index(start_point[0])]
+Frequency3 = Frequency[phase_constant.tolist().index(
+    start_point[1]): phase_constant.tolist().index(start_point[2])]
 
-# ax2 = fig.add_subplot(1, 2, 2)
-# ax2.set_xlabel("(b)   Frequency [Hz]")
-# ax2.set_ylabel("[rad/m]")
+beta4 = beta[phase_constant.tolist().index(start_point[2]): -1] - beta[phase_constant.tolist(
+).index(start_point[2])] - beta[phase_constant.tolist().index(start_point[0])]
+Frequency4 = Frequency[phase_constant.tolist().index(start_point[2]): -1]
+
+ax1.plot(Frequency, damping_constant, color="red")
+ax2.plot(Frequency, phase_constant, color="red", label="phase constant from actual data")
+ax2.plot(Frequency_gamma, phase_constant_linear, color="hotpink")
+ax2.plot(Frequency, beta, color="green")
+
+ax2.plot(Frequency1, beta1, color="blue", label="phase constant from the form (1)")
+ax2.plot(Frequency2, beta2, color="blue")
+ax2.plot(Frequency3, beta3, color="blue")
+ax2.plot(Frequency4, beta4, color="blue")
 
 
-# ax1.plot(Frequency, damping_constant, color="red" )
-# ax2.plot(Frequency, phase_constant, color="red")
-# ax2.plot(Frequency_gamma, phase_constant_linear, color="blue")
-# ax2.plot(Frequency, beta, color="green")
+fig.tight_layout()
 
-# fig.tight_layout()
+ax1.ticklabel_format(style='plain', axis='x')
+ax2.ticklabel_format(style='plain', axis='x')
 
-# ax1.ticklabel_format(style='plain',axis='x')
-# ax2.ticklabel_format(style='plain',axis='x')
+ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax1.ticklabel_format(style="sci",  axis="x", scilimits=(6, 6))
+ax2.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax2.ticklabel_format(style="sci",  axis="x", scilimits=(6, 6))
 
-# ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-# ax1.ticklabel_format(style="sci",  axis="x",scilimits=(6, 6))
-# ax2.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-# ax2.ticklabel_format(style="sci",  axis="x",scilimits=(6, 6))
+ax1.set_xlim([60000, 30000000])
+ax2.set_xlim([60000, 30000000])
 
-# ax1.set_xlim([60000, 30000000])
-# ax2.set_xlim([60000, 30000000])
+x_formatter = ScalarFormatter(useOffset=False)
+ax1.yaxis.set_major_formatter(x_formatter)
 
-# x_formatter = ScalarFormatter(useOffset=False)
-# ax1.yaxis.set_major_formatter(x_formatter)
+ax1.legend(loc='lower left')  # 凡例
+ax2.legend(loc='lower left')  # 凡例
 
-# plt.show()
+plt.show()
+
+# fig.savefig("./figure/phase.pdf")
