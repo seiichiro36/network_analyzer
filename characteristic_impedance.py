2b18e1272f559f58e157011b702ba83c7c9e68e6
Kouno <daigakuyouyyo@gmail.com>
2023-04-20 17:27:50 +0900

    伝播定数から求めた特性インピーダンスのグラフを出力するファイルを追加

--- characteristic_impedance.py
+++ characteristic_impedance.py
@@ -1,7 +1,5 @@
 from measurements_transform import Open, Short
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import ScalarFormatter
 
 
 open = Open("OPEN.CSV")
@@ -32,45 +30,6 @@ class Characteristic_impedance:
         return characteristic_impedance_real, characteristic_impedance_imag
 
 
-fig = plt.figure(figsize=(12, 8))
 # Frequency1 = np.linspace(60000, 30000000, 501)
 # np.linespaceからnp.arangeに変更
 Frequency = np.arange(60000, 30000001, 59880)
-
-characteristic_impedance = Characteristic_impedance(open_complex_data[2], short_complex_data[2])
-
-ax1 = fig.add_subplot(2, 1, 1)
-ax1.set_xlabel("Frequency [Hz]")
-ax1.set_ylabel("[Ω]")
-
-if __name__ == "__main__":
-    ax1.ticklabel_format(style='plain', axis='x')
-
-    ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-    ax1.ticklabel_format(style="sci",  axis="x", scilimits=(6, 6))
-
-    ax1.set_xlim([60000, 30000000])
-
-    x_formatter = ScalarFormatter(useOffset=False)
-    ax1.yaxis.set_major_formatter(x_formatter)
-
-    ax1.legend(loc='lower left')  # 凡例
-
-    ax1.plot(Frequency, characteristic_impedance.characteristic_impedance()[0], color="red",
-             label="calcuration wave by gamma real")
-
-    ax1.legend(loc='lower left')  # 凡例
-
-    plt.show()
-
-
-# if __name__ == "__main__":
-#     ax1.ticklabel_format(style='plain', axis='x')
-
-#     ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-#     ax1.ticklabel_format(style="sci",  axis="x", scilimits=(6, 6))
-
-#     ax1.set_xlim([60000, 30000000])
-
-#     x_formatter = ScalarFormatter(useOffset=False)
-#     ax1.yaxis.set_major_formatter(x_formatter)
--- characteristic_impedance_by_propagation_constant.py
+++ characteristic_impedance_by_propagation_constant.py
@@ -1,8 +1,15 @@
-from compare_actual_theoritical import x, y
+from Impedance_by_propagation_constant import x, y
 import matplotlib.pyplot  as plt
 import numpy as np 
 from characteristic_impedance import Characteristic_impedance
 from matplotlib.ticker import ScalarFormatter
+from measurements_transform import Open, Short
+
+open = Open("OPEN.CSV")
+open_complex_data = open.open_parameter()
+
+short = Short("SHORT.CSV")
+short_complex_data = short.short_parameter()
 
 
 fig = plt.figure(figsize=(12, 8))
@@ -10,6 +17,7 @@ fig = plt.figure(figsize=(12, 8))
 # np.linespaceからnp.arangeに変更
 Frequency = np.arange(60000, 30000001, 59880)
 
+characteristic_impedance = Characteristic_impedance(open_complex_data[2], short_complex_data[2])
 characteristic_impedance_by_gamma = Characteristic_impedance(x, y)
 
 
@@ -23,8 +31,10 @@ ax2.set_ylabel("[Ω]")
 
 
 ax1.plot(Frequency, characteristic_impedance_by_gamma.characteristic_impedance()[0], color="red", label="calcuration wave by gamma real")
+ax1.plot(Frequency, characteristic_impedance.characteristic_impedance()[0], color="blue", label="calcuration wave by gamma real")
 
 ax2.plot(Frequency, characteristic_impedance_by_gamma.characteristic_impedance()[1], color="red", label="calcuration wave by gamma imag")
+ax2.plot(Frequency, characteristic_impedance.characteristic_impedance()[1], color="blue", label="calcuration wave by gamma imag")
 
 fig.tight_layout()
 
@@ -33,7 +43,14 @@ ax1.ticklabel_format(style='plain', axis='x')
 ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
 ax1.ticklabel_format(style="sci",  axis="x", scilimits=(6, 6))
 
+ax2.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax2.ticklabel_format(style="sci",  axis="x", scilimits=(6, 6))
+# ax2.ticklabel_format(style="sci",  axis="y", scilimits=(-12, -12))
+
+
 ax1.set_xlim([60000, 30000000])
+ax2.set_xlim([60000, 30000000])
+# ax2.set_ylim([-1, 1])
 
 x_formatter = ScalarFormatter(useOffset=False)
 ax1.yaxis.set_major_formatter(x_formatter)
