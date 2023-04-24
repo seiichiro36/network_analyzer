e49b134ca98c264e24fad966756d764888f94d05
Kouno <daigakuyouyyo@gmail.com>
2023-04-24 22:19:33 +0900

    シミュレータ完成

--- simurator.py
+++ simurator.py
@@ -2,35 +2,33 @@ import tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 import numpy as np
-from actual_damping import actual_data
-import itertools
-
-
+from theoretical_propagation_constant import complex
 
 #スケール用関数
 def change(value):
-    t = float(value)
-    y = np.sqrt(t * x)
-    line.set_ydata(y)
+    length = float(value)
+    Zins = 50 * np.tanh(complex * length)
+    Zino = 50 / np.tanh(complex * length)
+    line1.set_ydata(Zins)
+    line2.set_ydata(Zino)
     canvas.draw()
     
 root = tkinter.Tk()
 root.title("matplotlib 埋め込み")
 
 #グラフデータ
-x = np.linspace(0, 300000000, 501)
-y = np.sqrt(x)
+Frequency = np.arange(60000, 30000001, 59880)
+y = 0 * Frequency
 
-y1 = actual_data
-y1 = list(itertools.chain.from_iterable(y1))
+# y1 = actual_data
+# y1 = list(itertools.chain.from_iterable(y1))
 
 
 #グラフ用オブジェクト生成
-fig = Figure(figsize=(12, 8))   #Figure
-ax = fig.add_subplot(1, 1, 1)           #Axes
-line, = ax.plot(x, y)                  #2DLine
-ax.plot(x, y1)                 
-
+fig = Figure(figsize=(12, 8)) 
+ax = fig.add_subplot(1, 1, 1)        
+line1, = ax.plot(Frequency, y)                 
+line2, = ax.plot(Frequency, y)                 
 
 #Figureを埋め込み
 canvas = FigureCanvasTkAgg(fig, root)
@@ -39,14 +37,19 @@ canvas.get_tk_widget().pack()
 
 #ツールバーを表示
 toolbar=NavigationToolbar2Tk(canvas, root)
-ax.set_xlim([0, 300000000])
-# ax.set_ylim([0, 0.08])
+# ax.set_xlim([0, 300000000])
+ax.set_ylim([0, 1000])
 
 #スケール
 s = tkinter.Scale(
     root,
     orient="horizontal",    #方向
     command=change,  #調整時に実行
+    width=30,
+    length=500,
+    from_=0,
+    to=30,
+    resolution=0.1
     )
 s.pack()
 
